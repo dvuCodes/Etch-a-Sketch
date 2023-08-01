f7bbcb2c66cb6822fea264ad45d57bab196e3b7e
sitncrush <89580762+dvuCodes@users.noreply.github.com>
2023-08-01 21:50:00 +1000

    Reworked the function to inejct the grid cells. We now properlly create the grids using a nested for loop

--- src/script.js
+++ src/script.js
@@ -1,1 +1,77 @@
 "use strict"
+const mainContainerEl = document.getElementById("main-container")
+const changeGridBtnEl = document.getElementById("change-grid-btn")
+const gridSizeDisplayEl = document.getElementById("grid-size-display")
+const gridInputEl = document.getElementById("grid-input")
+
+// Trigger creation of grid on DOM content loaded
+window.addEventListener("DOMContentLoaded", (e) => {
+  createGrids()
+})
+
+onLoad()
+
+changeGridBtnEl.addEventListener("click", (e) => {
+  e.preventDefault()
+  console.log(gridInputEl)
+  createGrids(getGridNumbers(gridInputEl.value))
+})
+
+function createGrids(num) {
+  const numberOfPixels = num || 16
+
+  //   calculate width and height of cells
+  let pixelWidth = 40 / numberOfPixels + "rem"
+  let pixelHeight = 40 / numberOfPixels + "rem"
+
+  //   Refence the main grid container=
+  const gridEl = document.getElementById("grid")
+
+  //   intiate array
+  let divArray = []
+
+  //   here we create a nested for loop for the basic grid structure of 16 x 16. For each div element we inject grid cells.
+  for (let i = 0; i < numberOfPixels; i++) {
+    // for each div created loop through and create nested divs
+    divArray[i] = document.createElement("div")
+
+    gridEl.appendChild(divArray[i])
+
+    for (let j = 0; j < numberOfPixels; j++) {
+      // iterate through loop creating divs
+      const newDiv = document.createElement("div")
+      // refernce a class attribute to create
+      const classAttribute = document.createAttribute("class")
+      classAttribute.value = "grid-cells"
+
+      // set the attribute of the newDiv to the assigned class attribute
+      newDiv.setAttributeNode(classAttribute)
+
+      // reference a width and height attribute for the divs
+      const widthHeightAttribute = document.createAttribute("style")
+      widthHeightAttribute.value = `width: ${pixelWidth}; height:${pixelHeight}`
+      newDiv.setAttributeNode(widthHeightAttribute)
+
+      // Add the new element to the DOM
+      divArray[i].appendChild(newDiv)
+    }
+  }
+}
+
+// const gridBoxEl = document.querySelectorAll(".grid-cells")
+
+// gridBoxEl.forEach((box) => {
+//   box.addEventListener("mouseenter", (e) => {
+//     e.target.classList.add("hover")
+//   })
+//   box.addEventListener("mouseleave", (e) => {
+//     e.target.classList.remove("hover")
+//   })
+// })
+
+function onLoad() {
+  const gridPixels = document.querySelectorAll("grid-cells")
+  gridPixels.forEach((grid) =>
+    grid.addEventListener("mouseenter", (e) => e.target.classList.add("hover"))
+  )
+}
